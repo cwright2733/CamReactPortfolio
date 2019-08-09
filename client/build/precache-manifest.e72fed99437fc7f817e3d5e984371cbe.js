9b5a0a5cb0de0741b264d530cafc4deee7e9cb6f
Cameron Wright <arabia.cw@gmail.com>
2019-08-09 15:31:38 -0400

    sidenav

---
+++ client/build/precache-manifest.e72fed99437fc7f817e3d5e984371cbe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aec7a4fa64d280e986e7f8139e850e5",
+    "url": "/CamReactPortfolio/index.html"
+  },
+  {
+    "revision": "b7850549ca257f4b7247",
+    "url": "/CamReactPortfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7684408bd5554961719d",
+    "url": "/CamReactPortfolio/static/js/2.4f057907.chunk.js"
+  },
+  {
+    "revision": "b7850549ca257f4b7247",
+    "url": "/CamReactPortfolio/static/js/main.d9ed891b.chunk.js"
+  },
+  {
+    "revision": "6e7d99804a276be14192",
+    "url": "/CamReactPortfolio/static/js/runtime~main.ae35cbd7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CamReactPortfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Header1 = {
+  color: 'white',
+  fontSize: '25px',
+  padding: '6px 8px 6px 16px'
+};
+
+let header3 ={
+  fontSize: '15px',
+  color: 'white',
+}
+
+
+
+class App extends Component {
+  render() {
+    return (
+
+         
+<div class="sidenav">
+
+  <h3 style={header3}>Cameron Wright</h3>
+
+        <div class="position"> 
+  <ul>
+  <h1 style={Header1}>Home</h1>
+  <h1 style={Header1}>About me</h1>
+  <h1 style={Header1}>Information</h1>
+  </ul>
+
+  </div>
+
+
+</div>
+
+
+
+
+      
+     
+    );
+  }
 }
 
 export default App;
