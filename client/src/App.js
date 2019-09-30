a187ffd794de5c2611872688726c8d3bf65caac6
Cameron Wright <arabia.cw@gmail.com>
2019-09-29 23:50:46 -0400

    react route links

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import plogo from '../src/images/pinterest-logo.png'
 
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./components/About";
+import Home from "./components/Home";
 
-const Header1 = {
-  color: 'white',
-  fontSize: '25px',
-  padding: '6px 8px 6px 16px'
-};
+
 
 let header3 ={
   fontSize: '15px',
@@ -20,35 +19,30 @@ let header3 ={
 class App extends Component {
   render() {
     return (
+      <Router>
 
          
-<div class="sidenav">
-    <img src={plogo} alt="Portlogo" />
-  <h3 style={header3}>Cameron Wright Portfolio</h3>
-      
+      <div class="sidenav">
+          <img src={plogo} alt="Portlogo" />
+          <h3 style={header3}>Cameron Wright Portfolio</h3>
+        <div class="position">
+          <ul>
+            <Link to="/" className="nav-Link " style={{textDecoration: 'none', color: 'white', fontSize: '25px', padding: '6px 8px 6px 16px', display: 'block'}}>Home</Link>
+            <Link to="/about" className="nav-Link" style={{textDecoration: 'none', color: 'white', fontSize: '25px', padding: '6px 8px 6px 16px', display: 'block'}}>About me</Link>
+            <Link to="/about" className="nav-Link" style={{textDecoration: 'none', color: 'white', fontSize: '25px', padding: '6px 8px 6px 16px', display: 'block'}}>Information</Link>
+          </ul>
+        </div>
 
 
-        <div class="position"> 
-  <ul>
-  <h1 style={Header1}>Home</h1>
-  <h1 style={Header1}>About me</h1>
-  <h1 style={Header1}>Information</h1>
-  </ul>
-
-  </div>
-
-  
-
-
-</div>
-
-
-
-
-      
-     
+        <Route path="/" exact component={Home} />
+        <Route path="/about" component={About} />
+        
+      </div> 
+      </Router>    
     );
   }
 }
+
+
 
 export default App;
